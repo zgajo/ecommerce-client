c4a0b043f9e9478f2c5d87ae1d4a1a338122f8c9
Darko Pranjic <darkopranjic.4@gmail.com>
2019-02-28 13:25:03 +0100

    Dynamic create products template updated

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -39,92 +39,94 @@ class Categories extends Component {
 
     return (
       <ResponsiveContainer>
-        <Segment style={{ padding: "8em 0em" }} vertical>
-          <Grid>
-            <Grid.Column width={4}>
-              <Menu fluid vertical tabular>
-                {data.ecommerce.categories &&
-                  data.ecommerce.categories.map(({ name }, index) => (
-                    <Menu.Item
-                      key={"category_" + index}
-                      name={name}
-                      active={
-                        data.sitePage.path.includes(name.toLowerCase()) ||
-                        index === 0
-                      }
-                      onClick={() =>
-                        navigate(
-                          `${
-                            data.site.siteMetadata.category_slug
-                          }/${name.toLowerCase()}/`
+        <Segment style={{ padding: "4em 0em" }} vertical>
+          <Grid container stackable verticalAlign="middle">
+            <Grid>
+              <Grid.Column width={4}>
+                <Menu fluid vertical tabular>
+                  {data.ecommerce.categories &&
+                    data.ecommerce.categories.map(({ name }, index) => (
+                      <Menu.Item
+                        key={"category_" + index}
+                        name={name}
+                        active={
+                          data.sitePage.path.includes(name.toLowerCase()) ||
+                          index === 0
+                        }
+                        onClick={() =>
+                          navigate(
+                            `${
+                              data.site.siteMetadata.category_slug
+                            }/${name.toLowerCase()}/`
+                          )
+                        }
+                      />
+                    ))}
+                </Menu>
+              </Grid.Column>
+
+              <Grid.Column stretched width={12}>
+                {/* <Item.Group> */}
+                <Grid columns="three">
+                  {data.ecommerce.categories &&
+                    data.ecommerce.categories.length &&
+                    data.ecommerce.categories[0].products.map(
+                      (product, index) => {
+                        return (
+                          <Grid.Column key={`active_${index}`}>
+                            <Dimmer.Dimmable
+                              as={Card}
+                              dimmed={this.state[`active_${index}`]}
+                              onMouseEnter={this.handleShow(`active_${index}`)}
+                              onMouseLeave={this.handleHide(`active_${index}`)}
+                            >
+                              <Image
+                                fluid={true}
+                                src={`${
+                                  process.env.SERVER_URL
+                                }/product_images/${product.image}`}
+                              />
+                              <Card.Content>
+                                <Card.Header>{product.name}</Card.Header>
+                                <Card.Description
+                                  className={styles.items_card_description}
+                                  content={product.description}
+                                />
+                              </Card.Content>
+                              <Card.Content extra>
+                                <a href=":;">
+                                  <Icon name="user" />
+                                  10 Friends
+                                </a>
+                              </Card.Content>
+
+                              <Dimmer
+                                active={this.state[`active_${index}`]}
+                                onClickOutside={this.handleHide(
+                                  `active_${index}`
+                                )}
+                                // verticalAlign="center"
+                              >
+                                <Grid textAlign="center">
+                                  <Grid.Row>
+                                    <Button positive>
+                                      <Icon name="shop" /> Add to basket
+                                    </Button>
+                                  </Grid.Row>
+                                  <Grid.Row>
+                                    <Button>View more</Button>
+                                  </Grid.Row>
+                                </Grid>
+                              </Dimmer>
+                            </Dimmer.Dimmable>
+                          </Grid.Column>
                         )
                       }
-                    />
-                  ))}
-              </Menu>
-            </Grid.Column>
-
-            <Grid.Column stretched width={12}>
-              {/* <Item.Group> */}
-              <Grid columns="three">
-                {data.ecommerce.categories &&
-                  data.ecommerce.categories.length &&
-                  data.ecommerce.categories[0].products.map(
-                    (product, index) => {
-                      return (
-                        <Grid.Column key={`active_${index}`}>
-                          <Dimmer.Dimmable
-                            as={Card}
-                            dimmed={this.state[`active_${index}`]}
-                            onMouseEnter={this.handleShow(`active_${index}`)}
-                            onMouseLeave={this.handleHide(`active_${index}`)}
-                          >
-                            <Image
-                              fluid={true}
-                              src={`${process.env.SERVER_URL}/product_images/${
-                                product.image
-                              }`}
-                            />
-                            <Card.Content>
-                              <Card.Header>{product.name}</Card.Header>
-                              <Card.Description
-                                className={styles.items_card_description}
-                                content={product.description}
-                              />
-                            </Card.Content>
-                            <Card.Content extra>
-                              <a href=":;">
-                                <Icon name="user" />
-                                10 Friends
-                              </a>
-                            </Card.Content>
-
-                            <Dimmer
-                              active={this.state[`active_${index}`]}
-                              onClickOutside={this.handleHide(
-                                `active_${index}`
-                              )}
-                              // verticalAlign="center"
-                            >
-                              <Grid textAlign="center">
-                                <Grid.Row>
-                                  <Button positive>
-                                    <Icon name="shop" /> Add to basket
-                                  </Button>
-                                </Grid.Row>
-                                <Grid.Row>
-                                  <Button>View more</Button>
-                                </Grid.Row>
-                              </Grid>
-                            </Dimmer>
-                          </Dimmer.Dimmable>
-                        </Grid.Column>
-                      )
-                    }
-                  )}
-              </Grid>
-              {/* </Item.Group> */}
-            </Grid.Column>
+                    )}
+                </Grid>
+                {/* </Item.Group> */}
+              </Grid.Column>
+            </Grid>
           </Grid>
         </Segment>
       </ResponsiveContainer>
--- src/templates/products.js
+++ src/templates/products.js
@@ -1,115 +1,131 @@
 import React, { Component } from "react"
 import { graphql, navigate } from "gatsby"
-import { Segment, Grid, Menu } from "semantic-ui-react"
+import {
+  Segment,
+  Grid,
+  Menu,
+  Button,
+  Icon,
+  Card,
+  Image,
+  Dimmer,
+} from "semantic-ui-react"
 import { ResponsiveContainer } from "../components/layout"
+import styles from "./products.module.css"
 
 class Categories extends Component {
+  state = {}
+
+  componentDidMount = () => {
+    const { data } = this.props
+    const keys = data.ecommerce.categories[0].products.map(
+      (product, index) => `active_${index}`
+    )
+
+    const actives = keys.reduce(
+      (obj, curr) => (obj = { ...obj, [curr]: false }),
+      {}
+    )
+
+    this.setState({ ...actives })
+  }
+
+  handleShow = key => () => this.setState({ [key]: true })
+  handleHide = key => () => this.setState({ [key]: false })
+
   render() {
     const { data } = this.props
 
     return (
       <ResponsiveContainer>
-        <Segment style={{ padding: "8em 0em" }} vertical>
-          <Grid>
-            <Grid.Column width={4}>
-              <Menu fluid vertical tabular>
-                {data.ecommerce.categories &&
-                  data.ecommerce.categories.map(({ name }, index) => (
-                    <Menu.Item
-                      key={`category_name_${index}`}
-                      name={name}
-                      // active={window.location.href.includes(name.toLowerCase())}
-                      onClick={() => {
-                        navigate(
-                          `${
-                            data.site.siteMetadata.category_slug
-                          }/${name.toLowerCase()}/`
-                        )
-                      }}
-                    />
-                  ))}
-              </Menu>
-            </Grid.Column>
+        <Segment style={{ padding: "4em 0em" }} vertical>
+          <Grid container stackable verticalAlign="middle">
+            <Grid>
+              <Grid.Column width={4}>
+                <Menu fluid vertical tabular>
+                  {data.ecommerce.categories &&
+                    data.ecommerce.categories.map(({ name }, index) => (
+                      <Menu.Item
+                        key={"category_" + index}
+                        name={name}
+                        active={
+                          data.sitePage.path.includes(name.toLowerCase()) ||
+                          index === 0
+                        }
+                        onClick={() =>
+                          navigate(
+                            `${
+                              data.site.siteMetadata.category_slug
+                            }/${name.toLowerCase()}/`
+                          )
+                        }
+                      />
+                    ))}
+                </Menu>
+              </Grid.Column>
 
-            <Grid.Column stretched width={12}>
-              <Segment>
-                This is an stretched grid column. This segment will always match
-                the tab height This is an stretched grid column. This segment
-                will always match the tab height This is an stretched grid
-                column. This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height This is an stretched grid column.
-                This segment will always match the tab height This is an
-                stretched grid column. This segment will always match the tab
-                height This is an stretched grid column. This segment will
-                always match the tab height
-              </Segment>
-            </Grid.Column>
+              <Grid.Column stretched width={12}>
+                {/* <Item.Group> */}
+                <Grid columns="three">
+                  {data.ecommerce.categories &&
+                    data.ecommerce.categories.length &&
+                    data.ecommerce.categories[0].products.map(
+                      (product, index) => {
+                        return (
+                          <Grid.Column key={`active_${index}`}>
+                            <Dimmer.Dimmable
+                              as={Card}
+                              dimmed={this.state[`active_${index}`]}
+                              onMouseEnter={this.handleShow(`active_${index}`)}
+                              onMouseLeave={this.handleHide(`active_${index}`)}
+                            >
+                              <Image
+                                fluid={true}
+                                src={`${
+                                  process.env.SERVER_URL
+                                }/product_images/${product.image}`}
+                              />
+                              <Card.Content>
+                                <Card.Header>{product.name}</Card.Header>
+                                <Card.Description
+                                  className={styles.items_card_description}
+                                  content={product.description}
+                                />
+                              </Card.Content>
+                              <Card.Content extra>
+                                <a href=":;">
+                                  <Icon name="user" />
+                                  10 Friends
+                                </a>
+                              </Card.Content>
+
+                              <Dimmer
+                                active={this.state[`active_${index}`]}
+                                onClickOutside={this.handleHide(
+                                  `active_${index}`
+                                )}
+                                // verticalAlign="center"
+                              >
+                                <Grid textAlign="center">
+                                  <Grid.Row>
+                                    <Button positive>
+                                      <Icon name="shop" /> Add to basket
+                                    </Button>
+                                  </Grid.Row>
+                                  <Grid.Row>
+                                    <Button>View more</Button>
+                                  </Grid.Row>
+                                </Grid>
+                              </Dimmer>
+                            </Dimmer.Dimmable>
+                          </Grid.Column>
+                        )
+                      }
+                    )}
+                </Grid>
+                {/* </Item.Group> */}
+              </Grid.Column>
+            </Grid>
           </Grid>
         </Segment>
       </ResponsiveContainer>
@@ -121,6 +137,9 @@ export default Categories
 
 export const query = graphql`
   query {
+    sitePage {
+      path
+    }
     site {
       siteMetadata {
         category_slug
