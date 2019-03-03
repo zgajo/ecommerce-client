e4efd186577ff70a192d71b4efad0fa42e3b0124
Darko Pranjic <darkopranjic.4@gmail.com>
2019-03-03 16:26:27 +0100

    Display inserted order details

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -75,14 +75,27 @@ export default class Cart extends Component {
     })
   }
 
+  orderDetailAttributeValues = od => {
+    const attributes = JSON.parse(od.attributes)
+
+    return Object.keys(attributes)
+      .map(attribute_id => {
+        console.log(attribute_id, attributes[attribute_id].value)
+        return attributes[attribute_id].value
+      })
+      .join(" ")
+  }
+
   render() {
+    const { order_details } = this.state
+
     return (
       <Popup
         trigger={
           <Button as="a" style={{ marginLeft: "0.5em" }}>
             <Icon name="cart" /> Cart
             <Label color="red" floating>
-              22
+              {order_details.length}
             </Label>
           </Button>
         }
@@ -91,37 +104,42 @@ export default class Cart extends Component {
         // open
       >
         <Item.Group divided>
-          <Item>
-            <Item.Image
-              size="tiny"
-              src="https://react.semantic-ui.com/images/wireframe/image.png"
-            />
+          {order_details.map((od, index) => (
+            <Item key={`order_detail_${index}`}>
+              <Item.Image
+                size="tiny"
+                src="https://react.semantic-ui.com/images/wireframe/image.png"
+              />
 
-            <Item.Content>
-              <Item.Extra style={{ margin: 0 }}>
-                <Button
-                  icon
-                  basic
-                  floated="right"
-                  style={{ marginTop: 0 }}
-                  size="tiny"
-                >
-                  <Icon name="remove" />
-                </Button>
-                <Header as="h3" style={{ marginTop: 0 }}>
-                  My Neighbor Totoro asdsadasdasdasasdasd
-                </Header>
-              </Item.Extra>
-              <Item.Meta className="cart_content_both_sides center_vertically">
-                <span className="cinema">IFC Cinema</span>
-                <span className="cinema">
-                  <Header as="h4" color="red">
-                    Price
+              <Item.Content>
+                <Item.Extra style={{ margin: 0 }}>
+                  <Button
+                    icon
+                    basic
+                    floated="right"
+                    style={{ marginTop: 0 }}
+                    size="tiny"
+                    onClick={() => this.removeFromCart(index)}
+                  >
+                    <Icon name="remove" />
+                  </Button>
+                  <Header as="h3" style={{ marginTop: 0 }}>
+                    My Neighbor Totoro asdsadasdasdasasdasd
                   </Header>
-                </span>
-              </Item.Meta>
-            </Item.Content>
-          </Item>
+                </Item.Extra>
+                <Item.Meta className="cart_content_both_sides center_vertically">
+                  <span className="cinema">{`${
+                    od.quantity
+                  } x ${this.orderDetailAttributeValues(od)}`}</span>
+                  <span className="cinema">
+                    <Header as="h4" color="red">
+                      ${decimalNumbersTwoDigits(od.quantity * od.unit_cost)}
+                    </Header>
+                  </span>
+                </Item.Meta>
+              </Item.Content>
+            </Item>
+          ))}
         </Item.Group>
       </Popup>
     )
